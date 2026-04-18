86ddc659adea26dc6d89d04cf6f7982cbd4d7bf2
Aneesh Abbineni <aabbineni@all.health>
2026-04-17 20:49:46 -0400

    Init

---
+++ README.md
@@ -0,0 +1,30 @@
+# Runclim
+
+Runclim runs ExoPlaSim, a GCM, on the cloud, and also has analysis tools. This is intended for use with worldbuilders.
+
+## Dependencies
+
+- Meson (for building)
+- C++20 (no need for module support)
+- Magick++
+- wgWidgets
+- Eigen
+
+## Building
+
+Clone the repository:
+
+```sh
+git clone https://github.com/arvillacl16-bit/runclim
+cd runclim
+```
+
+Now run:
+
+```sh
+meson setup builddir --buildtype=release
+meson compile -C builddir
+```
+
+The executable should be located at ./builddir/runclim.
+
---
+++ src/main.cpp
@@ -0,0 +1,46 @@
+#include <wx/wx.h>
+
+class MyApp : public wxApp {
+public:
+    bool OnInit() override;
+};
+
+class MyFrame : public wxFrame {
+private:
+    void OnButtonClick(wxCommandEvent& event);
+    void OnExit(wxCommandEvent& event);
+
+public:
+    MyFrame();
+};
+
+wxIMPLEMENT_APP(MyApp);
+
+bool MyApp::OnInit() {
+    MyFrame* frame = new MyFrame();
+    frame->Show(true);
+    frame->Raise();
+    frame->SetFocus();
+    return true;
+}
+
+MyFrame::MyFrame()
+    : wxFrame(nullptr, wxID_ANY, "Hello wxWidgets") {
+    wxPanel* panel = new wxPanel(this);
+    wxButton* button = new wxButton(panel, wxID_ANY, "Click me", wxPoint(100, 100));
+
+    Bind(wxEVT_BUTTON, &MyFrame::OnButtonClick, this, button->GetId());
+    Bind(wxEVT_MENU, &MyFrame::OnExit, this, wxID_EXIT);
+
+    CreateStatusBar();
+    SetStatusText("ready");
+}
+
+void MyFrame::OnButtonClick(wxCommandEvent& event) {
+    wxMessageBox("Hello world", "Test", wxOK | wxICON_INFORMATION);
+}
+
+void MyFrame::OnExit(wxCommandEvent& event) {
+    Close(true);
+}
+
